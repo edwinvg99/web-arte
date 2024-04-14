1d6fcd646e2f4a7fa96214a336712dbe54e0a531
edwinvg99 <velasquezgiraldoedwin@gmail.com>
2024-04-13 23:17:26 -0500

    actualizacion, modularizacion de tipos de arte completa

--- webArte/src/components/tiposArte.jsx
+++ webArte/src/components/tiposArte.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import CardTipoArtista from "./cardTipoArtista";
-export default function TiposArte({ titulo, descripcion }) {
+export default function TiposArte({ titulo, descripcion, children}) {
   return (
-    <section className="text-gray-400 body-font bg-gray-00 border-b-2 border-indigo-500 ">
+    <section className="text-gray-400 body-font border-b-2 border-indigo-500 ">
       <div className="container px-5 py-10 mx-auto mt-20 ">
         <div className="flex flex-wrap w-full mb-10">
           <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
@@ -16,41 +15,8 @@ export default function TiposArte({ titulo, descripcion }) {
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
-          <CardTipoArtista
-            urlImage={
-              "https://scontent-bog2-2.xx.fbcdn.net/v/t39.30808-1/427987804_930853691954342_7264008690099277796_n.jpg?stp=dst-jpg_p320x320&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=yJ_8JXiLz1EAb7GxL_0&_nc_ht=scontent-bog2-2.xx&oh=00_AfA__fk1HA7JeEc_2NWjSeYS-Y_bdrpm0gQtgNTB2FC_bQ&oe=66209154"
-            }
-            fecha="1997 † viviendo"
-            name="edwin velasquez"
-            descripcion="DESCRIPCION"
-          />
-          <CardTipoArtista
-            urlImage={
-              "https://scontent-bog2-2.xx.fbcdn.net/v/t39.30808-1/427987804_930853691954342_7264008690099277796_n.jpg?stp=dst-jpg_p320x320&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=yJ_8JXiLz1EAb7GxL_0&_nc_ht=scontent-bog2-2.xx&oh=00_AfA__fk1HA7JeEc_2NWjSeYS-Y_bdrpm0gQtgNTB2FC_bQ&oe=66209154"
-            }
-            fecha="1997 † viviendo"
-            name="edwin velasquez"
-            descripcion="DESCRIPCION"
-          />
-          <CardTipoArtista
-            urlImage={
-              "https://scontent-bog2-2.xx.fbcdn.net/v/t39.30808-1/427987804_930853691954342_7264008690099277796_n.jpg?stp=dst-jpg_p320x320&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=yJ_8JXiLz1EAb7GxL_0&_nc_ht=scontent-bog2-2.xx&oh=00_AfA__fk1HA7JeEc_2NWjSeYS-Y_bdrpm0gQtgNTB2FC_bQ&oe=66209154"
-            }
-            fecha="1997 † viviendo"
-            name="edwin velasquez"
-            descripcion="DESCRIPCION"
-          />
-          
-          <CardTipoArtista
-            urlImage={
-              "https://scontent-bog2-2.xx.fbcdn.net/v/t39.30808-1/427987804_930853691954342_7264008690099277796_n.jpg?stp=dst-jpg_p320x320&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=yJ_8JXiLz1EAb7GxL_0&_nc_ht=scontent-bog2-2.xx&oh=00_AfA__fk1HA7JeEc_2NWjSeYS-Y_bdrpm0gQtgNTB2FC_bQ&oe=66209154"
-            }
-            fecha="1997 † viviendo"
-            name="edwin velasquez"
-            descripcion="Iván Ivánovich Shishkin fue un pintor paisajista, grabador y
-                miembro fundador de la Sociedad de Exposiciones Artísticas
-                Itinerantes."
-          />
+        {children}
+
         </div>
       </div>
     </section>
--- webArte/src/pages/inicio.jsx
+++ webArte/src/pages/inicio.jsx
@@ -1,21 +1,99 @@
-import React from 'react'
-import TiposArte from '../components/tiposArte'
+import React from "react";
+import TiposArte from "../components/tiposArte";
+import CardTipoArtista from "../components/cardTipoArtista";
+export default function Inicio() {
+  const artistasRealismo = [
+    {
+      urlImage: "https://acortar.link/ptJn1J",
+      fecha: "1997 † 1877 ",
+      name: "GUSTAVE COURBET",
+      descripcion:
+        "Courbet combina diferentes elementos visuales. Utilizó tanto el cuchillo como el pincel, produciendo una superficie uniforme y ligeramente pastosa. Otros artistas adoptaron este tipo de técnica en diferentes variantes. ",
+    },
+    {
+      urlImage: "https://acortar.link/d6PjBV",
+      fecha: "1814  † 1875  ",
+      name: "JEAN-FRANÇOIS",
+      descripcion:
+        "CMillet fue nombrado Caballero de la Legión de Honor. A fines de la década de 1960, Jean-François Millet recibió el reconocimiento correcto por su trabajo, seguido por el éxito financiero. Sus obras han sido expuestas en exposiciones internacionales. ",
+    },
+    {
+      urlImage: "https://acortar.link/6gieeE",
+      fecha: "1815 † 1905",
+      name: " Adolph Friedrich",
+      descripcion:
+        "Con su técnica pictórica y su estilo personal, Menzel fue un artista adelantado a su tiempo. Su expresión pictórica libre y sus pinceladas abiertas no encontraron el favor de un público más amplio hasta más tarde.",
+    },
+    {
+      urlImage: "https://acortar.link/1pPIs9",
+      fecha: "1832 † 1898",
+      name: "Iwan Iwanowitsch",
+      descripcion: "Iván Ivánovich Shishkin fue un pintor paisajista, grabador y miembro fundador de la Sociedad de Exposiciones Artísticas Itinerantes.",
+    },
+  ];
 
-export default function Inicio (){
+  const artistasAbstracto = [
+    {
+      urlImage: "https://acortar.link/EaIA4Q",
+      fecha: "1955  † ... ",
+      name: "Hermann Angeli",
+      descripcion:
+        "Para el artista, la pintura abstracta e informal es una búsqueda constante de sí mismo, una inmersión en las profundidades de su alma para ver el mundo con otros ojos. Sólo a través de esta forma de pensar se puede llegar a conocerse a uno mismo y a sus sentimientos y pensamientos.",
+    },
+    {
+      urlImage: "https://acortar.link/JNecQ5",
+      fecha: "1879 † 1940",
+      name: "Paul Klee",
+      descripcion:"La pintura de Klee también incorpora elementos geométricos y simbólicos, con frecuentes referencias a la música e inspiraciones de sus viajes por diversas partes del mundo. Sin embargo, su vida estuvo marcada por la persecución nazi debido a sus raíces judías, influyendo significativamente en su producción artística.",
+    },
+    {
+      urlImage: "https://acortar.link/10B7zf",
+      fecha: "1872 † 1944",
+      name: "Piet Mondrian",
+      descripcion:"Este artista holandés es conocido por su estilo neoplasticista. Su forma de expresarse a través del uso de colores primarios y formas geométricas es tan extraordinario que Yves Saint Laurent diseñó vestidos inspirados en su trabajo. Dos de sus obras más reconocidas, Broadway Boogie-Woogie (1942-1943) y Composition in Planes (1914) se exhiben en el MoMA, y muchas de sus pinturas se encuentran repartidas por el mundo.",
+    },
+    {
+      urlImage: "https://acortar.link/SNTTQJ",
+      fecha: "1866  † 1944",
+      name: "Vasili Kandinsky",
+      descripcion:"Se cree que este artista ruso fue el impulsor del arte abstracto. Kandinsky tuvo una vida muy interesante: nació en Moscú, creció en Odessa, donde estudió arte, y más tarde regresó a Moscú para asistir a la universidad. Después, justo antes de la Revolución Rusa, se trasladó a Múnich. En Alemania, fue testigo del ascenso de los nazis al poder antes de trasladarse a Francia en 1933, donde vivió el resto de su vida. Sus obras, de diferentes estilos artísticos,",
+    },
+  ];
+
   return (
     <>
-    <TiposArte 
-      titulo='Estilo artístico Realismo'
-      descripcion='El Realismo es como un amor sincero por capturar la vida tal como es: sin adornos ni idealizaciones. Como un pintor dedicado a retratar cada detalle, el Realismo abraza la belleza de lo cotidiano y celebra la autenticidad de la experiencia humana.'
-    >
+      {/* Estilo Realismo */}
+      <TiposArte
+        titulo="Estilo artístico Realismo"
+        descripcion="El Realismo es un movimiento artístico y literario, originado en Francia a mitad del siglo XIX (1840-1880), cuyo propósito consistió en la representación objetiva de la realidad basándose en la observación de los aspectos cotidianos que brindaba la vida en la época."
+      >
+        {artistasRealismo.map(({ urlImage, fecha, name, descripcion , }) => (
+          <CardTipoArtista
+            key={name}
+            urlImage={urlImage}
+            fecha={fecha}
+            name={name}
+            descripcion={descripcion}
+            
+          />
+        ))}
+      </TiposArte>
 
-    </TiposArte>
-    <TiposArte 
-      titulo='Estilo Arte abstracto'
-      descripcion='El Arte Abstracto es como un romance con lo desconocido, donde las formas y colores danzan libremente en lienzo. Es una expresión del alma que desafía la realidad, invitándote a explorar emociones sin palabras y a descubrir significados en cada pincelada y línea.'
-    >
-
-    </TiposArte>
+      {/* Estilo Abstracto */}
+      <TiposArte
+        titulo="Estilo artístico Abstracto"
+        descripcion="El arte abstracto es el contrario del figurativo (es decir, la representación de objetos identificables mediante imágenes reconocibles). Por tanto la abstracción no representa «cosas» concretas de la naturaleza sino que propone una nueva realidad. Propone un «arte puro» mirando más allá de nuestra realidad."
+      >
+        {artistasAbstracto.map(({ urlImage, fecha, name, descripcion }) => (
+          <CardTipoArtista
+            key={name}
+            urlImage={urlImage}
+            fecha={fecha}
+            name={name}
+            descripcion={descripcion}
+          />
+        ))}
+      </TiposArte>
     </>
-  )
+  );
 }
